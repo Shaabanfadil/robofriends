81531be5dca7a0cc9aade7d846e23d31fe204216
Shaabanfadil <ewizzard100@gmail.com>
2022-11-27 14:49:55 -0500

    Update project files (creating cards)

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {robots} from "./robots"
+
+const card = (props) => {
+    //Destructure
+    const { id, name, email } = props;
+    return (     
+        <div className="bg-light-green dib br3 pa3 ma3 grow tc bw shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            
+            </div>
+        </div>
+    );
+}
+
+export default card;
